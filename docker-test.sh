68f68f7d737616a772a33a2625f7cbfaf8a3db7d
Johnny Hauser <johnnyhauser@gmail.com>
2021-05-26 07:49:11 -0500

    rm test docker container

--- docker-test.sh
+++ docker-test.sh
@@ -3,5 +3,5 @@ MYSQL_HOSTS="mysql80"
 
 for hostname in ${MYSQL_HOSTS}; do
   echo $hostname + node 12
-  docker run -it --network=zongji_default -e MYSQL_HOST=$hostname -w /build -v $PWD:/build node:12 npm test
+  docker run --rm -it --network=zongji_default -e MYSQL_HOST=$hostname -w /build -v $PWD:/build node:12 npm test
 done
